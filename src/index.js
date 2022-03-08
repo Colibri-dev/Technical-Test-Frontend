06d5376e6657b0bb4f24532356d6b8187cb2e23f
Fanny Perret <fanny.perret@viseo.com>
2022-03-08 10:09:32 +0100

    Clean project for test technical frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-new gridjs.Grid({
-    columns: [
-        "Name", 
-        "Email", 
-        "Phone Number"
-    ],
-    sort: true,
-    pagination: true,
-    fixedHeader: true,
-    data: [
-      ["John", "john@example.com", "(353) 01 222 3333"],
-      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-      ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-      ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-      ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
-    ] 
-}).render(document.getElementById("tableForBeer"));
+// Ajax call and Vanilla JS
+
