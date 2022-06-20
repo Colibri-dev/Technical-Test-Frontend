28c1cb24a7230e7e149977fb3f98eb3551db1f14
Marc Raes <contact@marcraes.fr>
2022-06-20 09:01:26 +0200

    Affichage de la map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Your Javascript here
+// Affichage de la carte
+var map = L.map('map', {
+    // center: [43.297493, 5.373192],
+    center: [43.29358673095703, 5.38786506652832],
+    zoom: 12
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
